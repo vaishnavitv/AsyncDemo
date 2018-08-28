54e17a78498bbe59638a81b56cb991baa99b109c
Vaishnavi T V <vaishnavi.subramanian@gmail.com>
2018-08-27 22:24:08 -0700

    Asynchronous Example in C#.

--- AsyncDemo/Program.cs
+++ AsyncDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncDemo
 {
@@ -6,7 +8,52 @@ namespace AsyncDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //await says: whatever I am running now, push to background, continue execution; If background is completed, run whatever was deferred.
+
+            //DemoAwait();
+            DemoAwaitTwo();
+            //DemoAwaitMultiple();
+            Console.WriteLine("Resume Execution ...");
+
+            Console.WriteLine("Press Key to End Program.");
+            Console.ReadLine();
         }
+
+        static async void DemoAwait()
+        {
+            //int result = WaitForSeconds(2); //Would block!
+            int result = await WaitForSecondsAsync(2);
+            Console.WriteLine($"Awaited Result: {result}");
+        }
+
+        static async void DemoAwaitTwo()
+        {
+            int result = await WaitForSecondsAsync(2) + await WaitForSecondsAsync(3);
+            Console.WriteLine($"Awaited Combined Result: {result}");
+        }
+
+        static void DemoAwaitMultiple() //Not Async!
+        {
+            int n = 10;
+            Task[] tasks = new Task[n];
+            for (; n > 0; --n)
+            {
+                tasks[n - 1] = WaitForSecondsAsync(n);
+            }
+            Task.WaitAll(tasks); //Roughly equivalent to: await tasks[0]; await tasks[1]; ... await tasks[n - 1];
+        }
+
+        static Task<int> WaitForSecondsAsync(int n)
+        {
+            return Task.Factory.StartNew(() => WaitForSeconds(n));
+        }
+
+        static int WaitForSeconds(int n)
+        {
+            Thread.Sleep(n * 1000);
+            Console.WriteLine($"Synchronously Waited for: {n}");
+            return n;
+        }
+
     }
 }
